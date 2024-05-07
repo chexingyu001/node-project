ba5a898e8a14a4e4af9a7497c96acc79ba7b96d5
lixiaoming1980 <boai-scholl@163.com>
2024-05-07 11:45:05 +0800

    root

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- 使用组件-组件标签 -->
+    <h1>hello world</h1>
+    <!-- 将值传给子组件,将方法传递给子组件,v-on:自定义事件,省略写法@decre="decrement" -->
+    <count :num="num" :incre="icrement" @decre="decrement"></count>
+    <!-- <count v-bing:num="num"></count> -->
+    <p>这是父组件的值{{num}}</p>
+  </div>
+</template>
+
+<script>
+  import count from './count.vue' // 导入组件（子组件）
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        num:10,   // 定义一个数据
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods:{
+      // 定义方法，修改自己的值
+      icrement(){
+        this.num++
+      },
+      decrement(){
+        this.num--
+      }
+    },
+    // 注册(映射)组件
+    components: {
+      count //
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/count.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- 组件：可以复用 -->
+    <button @click="increment">+</button>
+    <button v-on:click="decrement">-</button>
+    <p>子组件num {{num}} </p>
+    <p>props {{$props.num}} </p>
+    <!-- html赋值 -->
+    <p><span>html赋值</span><span v-html="num"></span></p>
+    <p><span>text赋值</span><span v-text="num"></span></p>
+    <!-- 表示 -->
+    <p>表达式：{{num % 2?'奇数':'偶数'}}</p>
+  </div>
+</template>
+
+<script>
+export default{
+  props:['num','incre'],   // 子组件接收值
+  data() {
+    return {
+      //num:0
+    }
+  },
+  methods:{
+    increment(){  // 加
+      this.incre()
+    },
+    decrement(){
+      // 自定义事件使用，不需要接收
+      this.$emit('decre')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
